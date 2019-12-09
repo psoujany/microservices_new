79519760db8de2e976faad75d65fa6e5238db281
psoujany <psoujany@in.ibm.com>
2019-12-09 18:05:35 +0530

    Add files via upload

--- currency-conversion-manage-service/currency-conversion-manage-service/src/main/java/com/microservices/currencyconversionmanageservice/CurrencyConversionManageController.java
+++ currency-conversion-manage-service/currency-conversion-manage-service/src/main/java/com/microservices/currencyconversionmanageservice/CurrencyConversionManageController.java
@@ -1,17 +1,18 @@
 package com.microservices.currencyconversionmanageservice;
 
-import java.util.Random;
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class CurrencyConversionManageController {
@@ -31,30 +32,25 @@ public class CurrencyConversionManageController {
 		
 	}
 	
-	@PostMapping("/currency-exchange")
-	public ExchangeValue addConversionFactor(@RequestBody ExchangeValue exchangevalue) {
-		repository.save(exchangevalue);	
-		return exchangevalue;
-		
+	@PostMapping(value="/currency-exchange-add")
+	public ResponseEntity<Object> addConversionFactor(@RequestBody ExchangeValue exchangevalue) {
+	    	repository.save(exchangevalue);
+			URI location = ServletUriComponentsBuilder
+					.fromCurrentRequest()
+					.path("/")
+					.buildAndExpand().toUri();
+
+					return ResponseEntity.created(location).build();
 	}
 	
 	@PostMapping("/currency-exchage-update")
 	public ExchangeValue updateConversionFactor(@RequestBody ExchangeValue exchangevalue) {
-		repository.save(exchangevalue);	
+		repository.save(exchangevalue);
 		return exchangevalue;
 	}
 	
-	@GetMapping("/test")
-	@HystrixCommand(fallbackMethod = "myFallbackMethod")
-	public @ResponseBody String test() {
-		if (new Random().nextBoolean()) {
-			return "Everything Working Fine";
-		} else {
-			throw new RuntimeException();
-		}
-	}
-	
-	public String myFallbackMethod() {
-		return "Fallback Enabled. Normal flow selected !!";
-	}
+	@GetMapping("/currency-exchange-all")
+	public List<ExchangeValue> findall(){
+	return repository.findAll();
+	} 
 }
